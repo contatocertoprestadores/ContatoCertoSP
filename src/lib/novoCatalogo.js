67f3a0412419166bfc2b6cd27989381826e976a9
Contato Certo SP New <contatocerto.prestadores@gmail.com>
2026-09-02 15:37:34 -0300

    Add files via upload

---
+++ src/lib/novoCatalogo.js
@@ -0,0 +1,136 @@
+
+export const NOVO_CATALOGO = [
+  {
+    categoria: "Guarda Roupa",
+    icone: "🛏️",
+    moveis: [
+      { id: "gd2", nome: "Guarda roupa 2 portas", novo: "R$ 150", usado: "R$ 120", desmontar: "R$ 100", novoNum: 150, usadoNum: 120, desmontarNum: 100 },
+      { id: "gd3", nome: "Guarda roupa 3 portas", novo: "R$ 160", usado: "R$ 130", desmontar: "R$ 110", novoNum: 160, usadoNum: 130, desmontarNum: 110 },
+      { id: "gd4", nome: "Guarda roupa 4 portas", novo: "R$ 170", usado: "R$ 140", desmontar: "R$ 120", novoNum: 170, usadoNum: 140, desmontarNum: 120 },
+      { id: "gd5", nome: "Guarda roupa 5 portas", novo: "R$ 180", usado: "R$ 150", desmontar: "R$ 130", novoNum: 180, usadoNum: 150, desmontarNum: 130 },
+      { id: "gd6", nome: "Guarda roupa 6 portas", novo: "R$ 190", usado: "R$ 160", desmontar: "R$ 140", novoNum: 190, usadoNum: 160, desmontarNum: 140 },
+      { id: "gd6gav", nome: "Guarda roupa 6 portas com gavetas", novo: "R$ 200", usado: "R$ 170", desmontar: "R$ 150", novoNum: 200, usadoNum: 170, desmontarNum: 150 },
+      { id: "gdCor", nome: "Guarda roupa portas de correr", novo: "R$ 220", usado: "R$ 170", desmontar: "R$ 150", novoNum: 220, usadoNum: 170, desmontarNum: 150 },
+      { id: "gdCorEsp", nome: "Guarda roupa portas de correr com espelho", novo: "R$ 230", usado: "R$ 180", desmontar: "R$ 150", novoNum: 230, usadoNum: 180, desmontarNum: 150 },
+      { id: "gdCanto", nome: "Guarda roupa de canto", novo: "R$ 250", usado: "R$ 200", desmontar: "R$ 180", novoNum: 250, usadoNum: 200, desmontarNum: 180 },
+      { id: "gdCloset", nome: "Guarda roupa grande/closet modulado", novo: "R$ 500", usado: "R$ 400", desmontar: "R$ 250", novoNum: 500, usadoNum: 400, desmontarNum: 250 },
+      { id: "gdPlan", nome: "Guarda roupa planejado", novo: "R$ 600", usado: "R$ 500", desmontar: "R$ 350", novoNum: 600, usadoNum: 500, desmontarNum: 350 }
+    ]
+  },
+  {
+    categoria: "Armário de Cozinha",
+    icone: "🍳",
+    moveis: [
+      { id: "aereoP", nome: "Armário aéreo pequeno", novo: "R$ 120", usado: "R$ 100", desmontar: "R$ 90", novoNum: 120, usadoNum: 100, desmontarNum: 90 },
+      { id: "aereoM", nome: "Armário aéreo médio", novo: "R$ 150", usado: "R$ 130", desmontar: "R$ 90", novoNum: 150, usadoNum: 130, desmontarNum: 90 },
+      { id: "aereoG", nome: "Armário aéreo grande", novo: "R$ 200", usado: "R$ 170", desmontar: "R$ 110", novoNum: 200, usadoNum: 170, desmontarNum: 110 },
+      { id: "coz1mod", nome: "Armário de cozinha 1 módulo", novo: "R$ 120", usado: "R$ 100", desmontar: "R$ 70", novoNum: 120, usadoNum: 100, desmontarNum: 70 },
+      { id: "coz2mod", nome: "Armário de cozinha 2 módulos", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "coz3mod", nome: "Armário de cozinha 3 módulos", novo: "R$ 250", usado: "R$ 300", desmontar: "R$ 170", novoNum: 250, usadoNum: 300, desmontarNum: 170 },
+      { id: "cozCompP", nome: "Armário de cozinha completo pequeno", novo: "R$ 400", usado: "R$ 500", desmontar: "R$ 280", novoNum: 400, usadoNum: 500, desmontarNum: 280 },
+      { id: "cozCompM", nome: "Armário de cozinha completo médio", novo: "R$ 600", usado: "R$ 750", desmontar: "R$ 400", novoNum: 600, usadoNum: 750, desmontarNum: 400 },
+      { id: "cozCompG", nome: "Armário de cozinha completo grande", novo: "R$ 800", usado: "R$ 1.000", desmontar: "R$ 550", novoNum: 800, usadoNum: 1000, desmontarNum: 550 },
+      { id: "cozPlan", nome: "Armário de cozinha planejado", novo: "R$ 900+", usado: "R$ 1.100+", desmontar: "R$ 600+", novoNum: 900, usadoNum: 1100, desmontarNum: 600 }
+    ]
+  },
+  {
+    categoria: "Balcão",
+    icone: "🗄️",
+    moveis: [
+      { id: "bal1", nome: "Balcão 1 porta", novo: "R$ 90", usado: "R$ 110", desmontar: "R$ 60", novoNum: 90, usadoNum: 110, desmontarNum: 60 },
+      { id: "bal2", nome: "Balcão 2 portas", novo: "R$ 110", usado: "R$ 130", desmontar: "R$ 70", novoNum: 110, usadoNum: 130, desmontarNum: 70 },
+      { id: "bal3", nome: "Balcão 3 portas", novo: "R$ 140", usado: "R$ 160", desmontar: "R$ 90", novoNum: 140, usadoNum: 160, desmontarNum: 90 },
+      { id: "bal4", nome: "Balcão 4 portas", novo: "R$ 170", usado: "R$ 200", desmontar: "R$ 110", novoNum: 170, usadoNum: 200, desmontarNum: 110 },
+      { id: "balGav", nome: "Balcão com gavetas", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "balGrande", nome: "Balcão de cozinha grande", novo: "R$ 220", usado: "R$ 270", desmontar: "R$ 150", novoNum: 220, usadoNum: 270, desmontarNum: 150 },
+      { id: "balPlan", nome: "Balcão planejado", novo: "R$ 250+", usado: "R$ 300+", desmontar: "R$ 180+", novoNum: 250, usadoNum: 300, desmontarNum: 180 }
+    ]
+  },
+  {
+    categoria: "Rack",
+    icone: "📺",
+    moveis: [
+      { id: "rackP", nome: "Rack pequeno", novo: "R$ 90", usado: "R$ 110", desmontar: "R$ 60", novoNum: 90, usadoNum: 110, desmontarNum: 60 },
+      { id: "rackM", nome: "Rack médio", novo: "R$ 120", usado: "R$ 140", desmontar: "R$ 80", novoNum: 120, usadoNum: 140, desmontarNum: 80 },
+      { id: "rackG", nome: "Rack grande", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "rackGav", nome: "Rack com gavetas/portas", novo: "R$ 160", usado: "R$ 190", desmontar: "R$ 110", novoNum: 160, usadoNum: 190, desmontarNum: 110 },
+      { id: "rackPainel", nome: "Rack com painel", novo: "R$ 200", usado: "R$ 240", desmontar: "R$ 140", novoNum: 200, usadoNum: 240, desmontarNum: 140 },
+      { id: "rackNichos", nome: "Rack com painel e nichos", novo: "R$ 250", usado: "R$ 300", desmontar: "R$ 170", novoNum: 250, usadoNum: 300, desmontarNum: 170 }
+    ]
+  },
+  {
+    categoria: "Painel de TV",
+    icone: "📺",
+    moveis: [
+      { id: "painelP", nome: "Painel pequeno", novo: "R$ 120", usado: "R$ 140", desmontar: "R$ 80", novoNum: 120, usadoNum: 140, desmontarNum: 80 },
+      { id: "painelM", nome: "Painel médio", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "painelG", nome: "Painel grande", novo: "R$ 200", usado: "R$ 240", desmontar: "R$ 130", novoNum: 200, usadoNum: 240, desmontarNum: 130 },
+      { id: "painelRack", nome: "Painel com rack", novo: "R$ 230", usado: "R$ 280", desmontar: "R$ 150", novoNum: 230, usadoNum: 280, desmontarNum: 150 },
+      { id: "painelNichos", nome: "Painel com nichos", novo: "R$ 220", usado: "R$ 270", desmontar: "R$ 150", novoNum: 220, usadoNum: 270, desmontarNum: 150 },
+      { id: "painelRipado", nome: "Painel ripado", novo: "R$ 250", usado: "R$ 300", desmontar: "R$ 170", novoNum: 250, usadoNum: 300, desmontarNum: 170 },
+      { id: "painelComplexo", nome: "Painel grande/complexo", novo: "R$ 300+", usado: "R$ 350+", desmontar: "R$ 200+", novoNum: 300, usadoNum: 350, desmontarNum: 200 }
+    ]
+  },
+  {
+    categoria: "Mesa",
+    icone: "🍽️",
+    moveis: [
+      { id: "mesaPeq", nome: "Mesa pequena", novo: "R$ 80", usado: "R$ 100", desmontar: "R$ 50", novoNum: 80, usadoNum: 100, desmontarNum: 50 },
+      { id: "mesaEsc", nome: "Mesa de escritório", novo: "R$ 90", usado: "R$ 110", desmontar: "R$ 60", novoNum: 90, usadoNum: 110, desmontarNum: 60 },
+      { id: "mesa4", nome: "Mesa de jantar 4 lugares", novo: "R$ 120", usado: "R$ 140", desmontar: "R$ 80", novoNum: 120, usadoNum: 140, desmontarNum: 80 },
+      { id: "mesa6", nome: "Mesa de jantar 6 lugares", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "mesa8", nome: "Mesa de jantar 8 lugares", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "mesaExt", nome: "Mesa extensível", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "mesaVidro", nome: "Mesa com vidro", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "mesa4cad", nome: "Mesa + 4 cadeiras", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "mesa6cad", nome: "Mesa + 6 cadeiras", novo: "R$ 220", usado: "R$ 270", desmontar: "R$ 150", novoNum: 220, usadoNum: 270, desmontarNum: 150 },
+      { id: "mesa8cad", nome: "Mesa + 8 cadeiras", novo: "R$ 280", usado: "R$ 330", desmontar: "R$ 180", novoNum: 280, usadoNum: 330, desmontarNum: 180 }
+    ]
+  },
+  {
+    categoria: "Sofá",
+    icone: "🛋️",
+    moveis: [
+      { id: "sofa2", nome: "Sofá 2 lugares simples", novo: "R$ 80", usado: "R$ 100", desmontar: "R$ 50", novoNum: 80, usadoNum: 100, desmontarNum: 50 },
+      { id: "sofa3", nome: "Sofá 3 lugares simples", novo: "R$ 100", usado: "R$ 120", desmontar: "R$ 60", novoNum: 100, usadoNum: 120, desmontarNum: 60 },
+      { id: "sofa4", nome: "Sofá 4 lugares", novo: "R$ 120", usado: "R$ 150", desmontar: "R$ 80", novoNum: 120, usadoNum: 150, desmontarNum: 80 },
+      { id: "sofaRet2", nome: "Sofá retrátil 2 lugares", novo: "R$ 130", usado: "R$ 160", desmontar: "R$ 90", novoNum: 130, usadoNum: 160, desmontarNum: 90 },
+      { id: "sofaRet3", nome: "Sofá retrátil 3 lugares", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "sofaRec", nome: "Sofá reclinável", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "sofaRetRecG", nome: "Sofá retrátil/reclinável grande", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "sofaMod", nome: "Sofá modular", novo: "R$ 180", usado: "R$ 230", desmontar: "R$ 120", novoNum: 180, usadoNum: 230, desmontarNum: 120 }
+    ]
+  },
+  {
+    categoria: "Cama",
+    icone: "🛏️",
+    moveis: [
+      { id: "camaSolt", nome: "Cama solteiro simples", novo: "R$ 90", usado: "R$ 110", desmontar: "R$ 60", novoNum: 90, usadoNum: 110, desmontarNum: 60 },
+      { id: "camaCasal", nome: "Cama casal simples", novo: "R$ 120", usado: "R$ 140", desmontar: "R$ 80", novoNum: 120, usadoNum: 140, desmontarNum: 80 },
+      { id: "camaQueen", nome: "Cama queen", novo: "R$ 140", usado: "R$ 170", desmontar: "R$ 90", novoNum: 140, usadoNum: 170, desmontarNum: 90 },
+      { id: "camaKing", nome: "Cama king", novo: "R$ 160", usado: "R$ 190", desmontar: "R$ 100", novoNum: 160, usadoNum: 190, desmontarNum: 100 },
+      { id: "camaGav", nome: "Cama com gavetas", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "camaBau", nome: "Cama com baú", novo: "R$ 160", usado: "R$ 200", desmontar: "R$ 110", novoNum: 160, usadoNum: 200, desmontarNum: 110 },
+      { id: "camaBoxCab", nome: "Cama box + cabeceira", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "beliche", nome: "Beliche", novo: "R$ 160", usado: "R$ 200", desmontar: "R$ 110", novoNum: 160, usadoNum: 200, desmontarNum: 110 },
+      { id: "treliche", nome: "Treliche", novo: "R$ 200", usado: "R$ 240", desmontar: "R$ 130", novoNum: 200, usadoNum: 240, desmontarNum: 130 },
+      { id: "bicama", nome: "Bicama", novo: "R$ 160", usado: "R$ 200", desmontar: "R$ 110", novoNum: 160, usadoNum: 200, desmontarNum: 110 },
+      { id: "camaInf", nome: "Cama infantil", novo: "R$ 100", usado: "R$ 120", desmontar: "R$ 70", novoNum: 100, usadoNum: 120, desmontarNum: 70 }
+    ]
+  },
+  {
+    categoria: "Penteadeira",
+    icone: "💄",
+    moveis: [
+      { id: "pentSimples", nome: "Penteadeira simples", novo: "R$ 100", usado: "R$ 120", desmontar: "R$ 70", novoNum: 100, usadoNum: 120, desmontarNum: 70 },
+      { id: "pent2gav", nome: "Penteadeira com 2 gavetas", novo: "R$ 120", usado: "R$ 140", desmontar: "R$ 80", novoNum: 120, usadoNum: 140, desmontarNum: 80 },
+      { id: "pent4gav", nome: "Penteadeira com 4 gavetas", novo: "R$ 150", usado: "R$ 180", desmontar: "R$ 100", novoNum: 150, usadoNum: 180, desmontarNum: 100 },
+      { id: "pent6gav", nome: "Penteadeira com 6 gavetas", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "pentCamarim", nome: "Penteadeira camarim", novo: "R$ 180", usado: "R$ 220", desmontar: "R$ 120", novoNum: 180, usadoNum: 220, desmontarNum: 120 },
+      { id: "pentCamarimEsp", nome: "Penteadeira camarim com espelho", novo: "R$ 220", usado: "R$ 270", desmontar: "R$ 150", novoNum: 220, usadoNum: 270, desmontarNum: 150 },
+      { id: "pentGrande", nome: "Penteadeira grande", novo: "R$ 250", usado: "R$ 300", desmontar: "R$ 170", novoNum: 250, usadoNum: 300, desmontarNum: 170 },
+      { id: "pentPlan", nome: "Penteadeira planejada", novo: "R$ 300+", usado: "R$ 350+", desmontar: "R$ 200+", novoNum: 300, usadoNum: 350, desmontarNum: 200 }
+    ]
+  }
+];
+export const TOTAL_MOVEIS = 77;
